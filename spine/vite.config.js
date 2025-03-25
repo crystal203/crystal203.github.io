282564f2645d811f4075480d379c109913221461
crystal203 <83070106+crystal203@users.noreply.github.com>
2025-03-25 21:10:39 +0800

    spine

---
+++ spine/vite.config.js
@@ -0,0 +1,20 @@
+import {fileURLToPath, URL} from 'node:url'
+
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: './',
+    plugins: [
+        vue(),
+    ],
+    server: {
+        port: 8192
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
+})
